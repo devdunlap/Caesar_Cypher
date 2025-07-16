3a086c1cdd6b13bf9a71b4836675664c44b2bf61
devdunlap <dev.dunlap.andrew@gmail.com>
2025-07-16 15:09:19 -0500

    added additional logic

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
+import art
+
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
-
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
 def caesar(original_text, shift_amount, direction):
     result_text = ""
@@ -36,4 +24,16 @@ def caesar(original_text, shift_amount, direction):
     else:
         print(result_text)
 
-caesar(text, shift, direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction not in ["encode", "decode"]:
+        print("Error: Please enter 'encode' or 'decode' for the direction.")
+    else:
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar(text, shift, direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+    if restart != "yes":
+        should_continue = False
+        print("Goodbye!")
